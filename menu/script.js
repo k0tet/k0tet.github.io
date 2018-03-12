d9516e7aa482abfc412185e7c9a79ee544f988b7
cniesen <cniesen@users.noreply.github.com>
2018-03-12 12:47:34 -0500

    updated google analytics to track hashbang

--- menu/script.js
+++ menu/script.js
@@ -46,10 +46,8 @@ function updateWebStats(page) {
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
     
-        ga('create', 'UA-12911574-9', 'auto');
-    } else {
-        console.log("ga exists, yeah!")
     }
     
+    ga('create', 'UA-12911574-9', 'auto');
     ga('send', 'pageview', page);
 }
